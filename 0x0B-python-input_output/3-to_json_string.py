0afc907b2a4429ebbe0c7cd289d2a9d74be2a5e3
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 21:23:53 +0100

    Update and rename 5-to_json_string.py to 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+A module that print json rep
+"""
 from json import dumps
 
 
 def to_json_string(my_obj):
+    """
+    function that print json representation
+    """
     return dumps(my_obj)
