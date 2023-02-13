08745b150fc57e039713fb56ecae77e3f3fc0b98
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 23:02:34 +0100

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+"""
+class
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """
+    class def
+    """
     def __init__(self, width, height):
+        """
+        def func
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
@@ -11,7 +20,13 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
+        """
+        func def
+        """
         return self.__width * self.__height
 
     def __str__(self):
+        """
+        def fun
+        """
         return '[Rectangle] ' + str(self.__width) + '/' + str(self.__height)
