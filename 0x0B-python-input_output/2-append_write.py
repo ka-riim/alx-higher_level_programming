b56d1e5f87ac66e5350326e4c7cdc4050fe0b683
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 21:19:02 +0100

    Update and rename 4-append_write.py to 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+A module that appends to a file
+"""
 def append_write(filename="", text=""):
+    """
+    A function that appends
+    """
     with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
