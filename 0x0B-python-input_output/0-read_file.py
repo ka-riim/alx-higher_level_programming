19a158815025aac51aa5f61401894f579732e0c2
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 21:09:59 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,9 @@
 A module that opens and read a file
 """
 def read_file(filename=""):
+    """
+    a function that opens and prints a file
+    """
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
