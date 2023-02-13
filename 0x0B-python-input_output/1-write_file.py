4c10379cb192c6d3a9ee147335ee83bd3d7bfd67
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 21:16:46 +0100

    Update and rename 3-write_file.py to 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+A module that writes to a file
+"""
 def write_file(filename="", text=""):
+    """
+    a function that writes to a file
+    """
     with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)
