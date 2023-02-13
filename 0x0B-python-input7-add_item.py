5a7bd5598bf940485b3ae5183bba7016d6baf280
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 21:36:57 +0100

    Update and rename 0x0B-python-input_output/9-add_item.py to 0x0B-python-input7-add_item.py

--- 0x0B-python-input7-add_item.py
+++ 0x0B-python-input7-add_item.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+a module that add augments and save to a file
+"""
 from os import path
 from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
