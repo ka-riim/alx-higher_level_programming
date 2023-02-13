62feea2ba58fb32d75774887a5d97ca7ff73ecb0
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 21:34:20 +0100

    Update and rename 8-load_from_json_file.py to 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+A module that create obj
+"""
 from json import loads
 
 
 def load_from_json_file(filename):
+    """
+    function that create obj
+    """
     with open(filename, encoding='utf-8') as f:
         return loads(f.read())
