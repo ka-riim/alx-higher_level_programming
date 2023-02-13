7ce837f0a0bd83759db3674c67f320419850e3e1
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 21:26:37 +0100

    Update and rename 6-from_json_string.py to 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+A module that returns obj
+"""
 from json import loads
 
 
 def from_json_string(my_str):
+    """
+    function that return obj
+    """
     return loads(my_str)
