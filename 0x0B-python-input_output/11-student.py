a4ab6300893f3d61c2b209a7d52cf2e7b3bd09b7
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 22:13:40 +0100

    Update and rename 13-student.py to 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
+"""
+class
+"""
+
+
 class Student:
     def __init__(self, first_name, last_name, age):
+        """
+        struct def
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """
+        struct def
+        """
         class_d = self.__dict__
         sel_d = dict()
 
@@ -22,6 +33,9 @@ class Student:
         return class_d
 
     def reload_from_json(self, json):
+        """
+        struct def
+        """
         for i in json:
             if i in self.__dict__.keys():
                 self.__dict__[i] = json[i]
