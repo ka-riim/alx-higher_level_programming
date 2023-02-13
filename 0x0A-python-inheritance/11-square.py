70d93db28548d66149eaa524097ebd8b04d1d21e
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 23:04:26 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
+"""
+module
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """
+    class
+    """
     def __init__(self, size):
+        """
+        def func
+        """
         self.integer_validator(size, size)
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
+        """
+        def func
+        """
         return '[Square] ' + str(self.__size) + '/' + str(self.__size)
