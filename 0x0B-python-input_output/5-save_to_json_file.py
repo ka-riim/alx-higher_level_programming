6091760e13736be7973c26ac80c1b4f177cb46a9
ka-riim <113716749+ka-riim@users.noreply.github.com>
2023-02-13 21:29:43 +0100

    Update and rename 7-save_to_json_file.py to 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+A module that save to json
+"""
 from json import dumps
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    function that saves to json
+    """
     with open(filename, mode='w', encoding='utf-8') as f:
         f.write(dumps(my_obj))
